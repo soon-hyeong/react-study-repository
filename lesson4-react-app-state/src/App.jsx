92a9874c989d97f62f81023dbfdc9b08e4f50b57
soon-hyeong <shjoo820@naver.com>
2025-09-11 15:29:41 +0900

    feat : react State Hook

---
+++ lesson4-react-app-state/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App() {
+	return (
+		<div>
+			<h1>useState Hook 기초공부</h1>
+			<Counter />
+		</div>
+	);
+}
+
+export default App;
