ae4b9b0323af967a90ddf6539198204f7b73ea1d
soon-hyeong <shjoo820@naver.com>
2025-09-16 11:20:49 +0900

    feat:lesson5 login

--- lesson5-react-app-conditional-rendering/src/components/LoginSystem.jsx
+++ lesson5-react-app-conditional-rendering/src/components/LoginSystem.jsx
@@ -22,6 +22,23 @@ function LoginSystem() {
 	// 로그인 상태 관리 : React Hook ( 상태 관리, 라이프 사이클 등의 기능을 제공 )
 	// isLoggedIn : 상태 저장 변수, setIsLoggedIn : 상태 변경을 위해 호출할 함수
 	const [isLoggedIn, setIsLoggedIn] = useState(false); // false 초기값
+	// 회원 이름 관리하는 state hook
+	// username : 상태 저장(회원명) 변수, setUsername : 상태 변경을 위해 호출될 함수
+	const [username, setUsername] = useState(""); //'' : 초기값
+
+	// 로그인 처리 함수
+	const handleLogin = () => {
+		const name = prompt("회원 이름을 입력하세요");
+		if (name) {
+			setUsername(name); // React state hook 을 이용해 username을 할당하여 리렌더링되어 보여짐
+			setIsLoggedIn(true); // React state hook을 이용해 loggedIn 할당하여 리렌더링되어 보여짐
+		}
+	};
+	// 로그아웃 처리 함수
+	const handleLogout = () => {
+		setUsername(""); // state hook 함수 이용해 정보 변경
+		setIsLoggedIn(false); // state hook 함수 이용 정보 변경
+	};
 
 	// jsx 화면 렌더링 정보 리턴
 	return (
@@ -31,16 +48,21 @@ function LoginSystem() {
 			{isLoggedIn ? (
 				// 로그인된 상태의 화면
 				<div className="logged-in">
-					<h3>환영합니다! 님</h3>
+					<h3>환영합니다! {username}님</h3>
 					<p>로그인이 완료되었습니다.</p>
-					<button className="logout-btn">로그아웃</button>
+					<button className="logout-btn" onClick={handleLogout}>
+						로그아웃
+					</button>
 				</div>
 			) : (
 				// 로그아웃된 상태의 화면
 				<div className="logged-out">
 					<h3>로그인이 필요합니다</h3>
 					<p>서비스를 이용하려면 로그인하세요.</p>
-					<button className="login-btn">로그인</button>
+					{/* onClick={handleLogin} : 클릭 이벤트 발생했을 때 실행될 함수 객체를 등록 */}
+					<button className="login-btn" onClick={handleLogin}>
+						로그인
+					</button>
 				</div>
 			)}
 			{/* 로그인 되었을 때만 아래 요소 렌더링
@@ -54,6 +76,7 @@ function LoginSystem() {
 
 			<div className="debug">
 				<p>현재 로그인 상태: {isLoggedIn ? "true" : "false"}</p>
+				<p>사용자 이름: {username}</p>
 				{/* <p>사용자 이름: </p> */}
 				{/* 내일 이어서 */}
 			</div>
